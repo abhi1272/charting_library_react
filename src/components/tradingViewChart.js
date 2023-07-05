ac6e1a0e21eacb66e56acde1355a6887d689be14
Abhishek kumar <abhishek.omex@gmail.com>
2023-07-05 19:13:18 +0530

    added code for react charting library

--- src/components/tradingViewChart.js
+++ src/components/tradingViewChart.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Datafeed from "../trading-view/datafeed";
-import TradingView from "../charting_library/charting_library.standalone";
+import {TradingView} from "../charting_library/charting_library.standalone";
 
 const TradingViewChart = () => {
   useEffect(() => {
@@ -10,12 +10,28 @@ const TradingViewChart = () => {
     document.head.appendChild(script);
 
     window.tvWidget = new TradingView.widget({
-      symbol: "Bitfinex:BTC/USD", // default symbol
-      interval: "1D", // default interval
-      fullscreen: true, // displays the chart in the fullscreen mode
-      container: "tv_chart_container",
+      symbol: "Monetax:AMC/USDT", // Default symbol pair
+      interval: "1", // Default interval
+      fullscreen: true, // Displays the chart in the fullscreen mode
+      container: "tv_chart_container", // Reference to an attribute of a DOM element
       datafeed: Datafeed,
       library_path: "/charting_library/",
+      disabled_features: [
+        "use_localstorage_for_settings",
+        "left_toolbar",
+        "header_widget",
+        "timeframes_toolbar",
+        "edit_buttons_in_legend",
+        "context_menus",
+        "control_bar",
+        "border_around_the_chart",
+      ],
+      overrides: {
+        "paneProperties.background": "#222222",
+        "paneProperties.vertGridProperties.color": "#454545",
+        "paneProperties.horzGridProperties.color": "#454545",
+        "scalesProperties.textColor": "#AAA",
+      },
     });
     //Do not forget to remove the script on unmounting the component!
     return () => script.remove();
