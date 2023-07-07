10977b0b1f1b9bdd3a1f9a624ff710c9f5472ba8
Abhishek kumar <abhishek.omex@gmail.com>
2023-07-07 15:43:54 +0530

    added code for new backend plesk

--- src/trading-view/webStreaming.js
+++ src/trading-view/webStreaming.js
@@ -1,6 +1,6 @@
 
 import { io } from "socket.io-client";
-const socket = io('wss://charting-lib-backend.vercel.app/');
+const socket = io('wss://api.monetaxexchange.com:8000/');
 //const socket = io('ws://monetaxexchange.com:8000');
 const channelToSubscription = new Map();
 
